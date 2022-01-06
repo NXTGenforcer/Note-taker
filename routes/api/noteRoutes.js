3aa77d0e4430a887788409e793f8a17aedc9178b
NXTGenforcer <gskywalker11@gmail.com>
2022-01-06 18:39:34 -0500

    Test push for heroku

---
+++ routes/api/noteRoutes.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const fs = require("fs");
+
+router.get("/", (req, res) => {
+  fs.readFile("../../db/db.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(400).json(err);
+    }
+    res.status(200).json(JSON.parse(data));
+  });
+});
+
+router.post("/", (req, res) => {
+  fs.readFile("../../db/db.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(400).json(err);
+    }
+    const notes = JSON.parse(data);
+    const newNote = req.body;
+    notes.push(newNote);
+
+    fs.writeFile("../../db/db.json", JSON.stringify(notes), (err) => {
+      if (err) {
+        res.status(400).json(err);
+      }
+    });
+    res.status(200).json(newNote);
+  });
+});
